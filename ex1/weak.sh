1ff48aab90a6305f9e36c18bd88a3ff5ad5c85b0
Simone Amadio <amadiosimone1996@gmail.com>
2018-10-14 17:57:55 +0200

    First weak result 10^9

--- ex1/weak.sh
+++ ex1/weak.sh
@@ -1,6 +1,9 @@
-echo "Weak scalability_test, base size=$1" 		#$1 indica il primo parametro, che metti dopo il .x
-for procs in 1 2 4 8 16 20 32 64 ; do
+cd hpc/ex1/
+echo "Weak scalability_test, base size=1000000000" 		#$1 indica il primo parametro, che metti dopo il .x
+for procs in 1 2 4 8 16 20; do
+# sync; echo 3 > /proc/sys/vm/drop_caches  
  echo "np=${procs}" 		
- /usr/bin/time mpirun -np ${procs} ./parallel.x $1
+ /usr/bin/time mpirun -np ${procs} ./parallel.x 1000000000
  echo "------------------------------------------------"
  done
+
