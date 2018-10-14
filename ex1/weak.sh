1e0c66ef7107ade7c143a9d2e71c3504493b987f
Simone Amadio <amadiosimone1996@gmail.com>
2018-10-14 16:12:16 +0200

    explication

--- ex1/weak.sh
+++ ex1/weak.sh
@@ -4,3 +4,5 @@ for procs in 1 2 4 8 16 20 32 64 ; do
  usr/bin/time mpirun -np ${procs} ./parallel.x $1
  echo "------------------------------------------------"
  done
+ 
+ #this is weak because in input there's 10^9 and the C program uses 10^9 * np, so the size of the problem increases along with the number of processors
