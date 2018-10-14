6c1253aad44485c8785fd9a89788233d5454fc15
Simone Amadio <amadiosimone1996@gmail.com>
2018-10-14 17:40:11 +0200

    back to normal

--- ex1/weak.sh
+++ ex1/weak.sh
@@ -1,6 +1,6 @@
-echo "Weak scalability_test, base size=10^9" 		#$1 indica il primo parametro, che metti dopo il .x
+echo "Weak scalability_test, base size=$1" 		#$1 indica il primo parametro, che metti dopo il .x
 for procs in 1 2 4 8 16 20 32 64 ; do
  echo "np=${procs}" 		
- /usr/bin/time mpirun -np ${procs} ./parallel.x 1000000000
+ /usr/bin/time mpirun -np ${procs} ./parallel.x $1
  echo "------------------------------------------------"
  done
