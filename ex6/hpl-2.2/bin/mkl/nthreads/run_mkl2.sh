ce691ba8dab0e3b9a957282b90f07682eaac742b
Simone Amadio <amadiosimone1996@gmail.com>
2018-11-12 21:42:11 +0100

    1t,2t done

--- ex6/hpl-2.2/bin/mkl/nthreads/run_mkl2.sh
+++ ex6/hpl-2.2/bin/mkl/nthreads/run_mkl2.sh
@@ -2,29 +2,15 @@
 
 /bin/hostname #name of the node which is executing my job
 #enter in the right position
-cd hpc/ex6/hpl-2.2/bin/mkl
+cd hpc/ex6/hpl-2.2/bin/mkl/nthreads
 
 #load modules needed
 module load mkl
 module load openmpi/1.8.3/intel/14.0
 
-#run the code
-
-mpirun -np 20 ./xhpl
-
-echo "---------------"
-echo "---------------"
-echo "---------------------------------------------------------------------------------"
-echo "               NOW THE INTEL ONE	            "
-echo "       "
-echo "   "
-echo " "
-echo " "
-echo " "
-echo " "
-
-#now the intel
-
+# intel
+set OMP_NUM_THREADS=4
+set np=20/OMP_NUM_THREADS
 ./xlinpack_xeon64 lininput_xeon64
 
 exit
