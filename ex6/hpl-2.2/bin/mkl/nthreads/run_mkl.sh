7c8a06f48166ec9d71c195d7696b6290d0d34d53
Simone Amadio <amadiosimone1996@gmail.com>
2018-11-22 00:38:56 +0100

    Script working?weird file

--- ex6/hpl-2.2/bin/mkl/nthreads/run_mkl.sh
+++ ex6/hpl-2.2/bin/mkl/nthreads/run_mkl.sh
@@ -13,41 +13,44 @@ for nproc in 1 2 4 5 10 20; do
 echo "nproc=${nproc}"
 echo " "
 echo " "
-export OMP_NUM_THREADS=$((20/nproc));
 mem=$((64512*20/nproc));
 
 P=1
 #setting P (and Q)for every number of processes
 case $nproc in
 	1)
-		let P=1
+		P=1
 	;;
 	2)
-		let P=2
+		P=2
 	;;
 	4)
-		let P=2
+		P=2
 	;;
 	5)
-                let P=5
+                P=5
         ;;
         10)
-                let P=5
+                P=5
         ;;
         20)
-        	let P=5
+        	P=5
        	;;
 	*)
-		echo "don\'t know"
+		echo  "don\'t know"
 	;;
 esac
 
 Q=$((nproc/P));
-sed -i '6\/.*/\"$mem        Ns\"/g' HPL.dat
-#sed -i -e "6/.*/$mem      Ns/" HPL.dat #replace memory asked with mem variable
+#sed -i -e '6 s/*/'${mem}'       Ns/' HPL.dat
+
+sed -i '6 c '${mem}'         Ns' HPL.dat
 #replace P and Q line:
-#sed -i '11/.*/\"$P            Ps\"/' HPL.dat
-#sed -i '12/.*/$Q            Qs/g' HPL.dat
+
+sed -i '11 c '${P}'         Ps' HPL.dat
+sed -i '12 c '${Q}'         Qs' HPL.dat
+#sed -i -e '11 s/*/'${P}'        Ps/' HPL.dat
+#sed -i -e '12 s/*/'${Q}'        Qs/' HPL.dat
 #and hopefully...
 
 echo " "
@@ -55,7 +58,8 @@ echo " "
 echo "-----------------------------------------------------------"
 echo " "
 echo " "
+
+export OMP_NUM_THREADS=$((20/nproc));
 mpirun -np ${nproc} ./xhpl
 done
 exit
-
