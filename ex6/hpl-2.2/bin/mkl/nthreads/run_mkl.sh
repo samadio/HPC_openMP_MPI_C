0284d91c04cc45fbffacfd16e53d93ab67ccd172
Simone Amadio <amadiosimone1996@gmail.com>
2018-11-21 11:17:44 +0100

    Bash experiment

--- ex6/hpl-2.2/bin/mkl/nthreads/run_mkl.sh
+++ ex6/hpl-2.2/bin/mkl/nthreads/run_mkl.sh
@@ -14,14 +14,47 @@ echo "nproc=${nproc}"
 echo " "
 echo " "
 export OMP_NUM_THREADS=$((20/nproc));
-mem=$((64512/OMP_NUM_THREADS));
-sed -i -e "s/64512/$mem/g" HPL.dat
-mpirun -np ${nproc} ./xhpl 
+mem=$((64512/nproc));
+
+#setting P (and Q)for every number of processes
+case proc in
+	1)
+		P=1
+	;;
+	2)
+		P=2
+	;;
+	4)
+		P=2
+	;;
+	5)
+                P=5
+        ;;
+        10)
+                P=5
+        ;;
+        20)
+        	P=5
+       	;;
+	*)
+		echo "don\'t know"
+	;;
+esac
+
+Q=$((np/P));
+
+sed -i -e "s/64512/$mem/g" HPL.dat #replace memory asked with mem variable
+#replace P and Q line:
+sed -i '11/.*/$P            Ps/' HPL.dat
+sed -i '12/.*/$Q            Qs/' HPL.dat
+#and hopefully...
+
+echo " "
+echo " "
+echo "-----------------------------------------------------------"
+echo " "
+echo " "
+mpirun -np ${nproc} ./xhpl
 done
 exit
 
-#for procs in 1 2 4 8 16 20; do
-# echo "np=${procs}"
-#  /usr/bin/time mpirun -np ${procs} ./parallel.x 1000000000 | grep "walltime" | $
-#   echo "------------------------------------------------" 
-#    done
