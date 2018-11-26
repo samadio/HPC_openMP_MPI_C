a231ad7895aad31e8ed2e9c6e9ee91714b982161
Simone Amadio <amadiosimone1996@gmail.com>
2018-11-26 14:38:35 +0100

    PIng Pong Same node done

--- ex5/omp_101.c
+++ ex5/omp_101.c
@@ -6,8 +6,8 @@ int main()  {
     {
     #if defined (_OPENMP) 
        np = omp_get_num_threads(); 
-          iam = omp_get_thread_num();
-          #endif
-            printf( "Hello from thread %d out of %d \n", iam, np);
-             }
-             }
+       iam = omp_get_thread_num();
+    #endif
+    printf( "Hello from thread %d out of %d \n", iam, np);
+    }
+}
