f4f10b583bcb4a3e3382f8edcab38a651875df96
Simone Amadio <amadiosimone1996@gmail.com>
2018-12-06 12:20:28 +0100

    Recompiled and boh

--- ex5/PingPong/nodes/ping.sh
+++ ex5/PingPong/nodes/ping.sh
@@ -1,12 +1,11 @@
 module load impi-trial/5.0.1.035 
+module load openmpi/1.8.3/intel/14.0
 module load hwloc
+module load intel/14.0
 
-cd /home/samadio/hpc/ex5/PingPong/nodes
-#for tries in 1 2 3 4 5 6 7 8 9 10; do
-#mpirun -np 2 -ppn 1 ./IMB-MPI1 PingPong >> ppn.txt
-
-mpirun -np 40  --map-by ppr:20:node hostname >> np40ppn20.txt
-#mpirun -np 2 --map-by node IMB-MPI1 PingPong >> bynode.txt
-
+#filename=$(echo "diffsocket$1.txt")
+#for tries in 1 2 3; do
+mpirun -np 2 IMB-MPI1 -map 1x2 -multi 0 PingPong
 #done
+
 exit
