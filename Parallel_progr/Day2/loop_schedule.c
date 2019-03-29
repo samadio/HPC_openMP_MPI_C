fedf34938cc68c82c91e71ccc0092d3c13dfc0d4
Simone Amadio <amadiosimone1996@gmail.com>
2019-03-29 12:22:44 +0100

    1st ex

--- Parallel_progr/Day2/loop_schedule.c
+++ Parallel_progr/Day2/loop_schedule.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<omp.h>
 
 void print_usage( int * a, int N, int nthreads ) {
 
@@ -18,8 +19,34 @@ void print_usage( int * a, int N, int nthreads ) {
 }
 
 int main( int argc, char * argv[] ) {
+  const int N=250;
+  int a[N];
+  int nthreads=10;
 
-  // TODO                                                                                                                                                                                                                                                                                
+  printf("static\n");
+  for(int chunk=1,chunk<11,chunk=chunk+9){
+    #pragma omp parallel
+    {
+      #pragma omp single //only one should execute this, no matter who
+      {
+        printf("chunk= %d\n",chunk);
+      }
 
+      int thread_id=omp_get_thread_num(); //identifier
+      
+      #pragma omp for schedule(static,chunk)//implicit barrier is placed
+      for(int i = 0; i < N; i++)
+      {
+        a[i]=thread_id;
+      }
+      
+      //only one should execute this, no matter who
+      #pragma omp single
+      {
+        print_usage(a,N,nthreads);
+        printf("\n");
+      }
+    }
+  }  
   return 0;
 }
