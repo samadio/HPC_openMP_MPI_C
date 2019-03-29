dd1948f6ebed0d60c4c5f7df04e857b0c3b21705
Simone Amadio <amadiosimone1996@gmail.com>
2019-03-29 17:02:44 +0100

    finished

--- Parallel_progr/Day2/ex1/parallel.c
+++ Parallel_progr/Day2/ex1/parallel.c
@@ -11,11 +11,11 @@ int main(){
   size_t N=1000000000;
   double h=1.0/(N);
   double pi=0.0;
-  int nthr= omp_get_num_thread();
+  int nthr= omp_get_num_threads();
   double tstart=omp_get_wtime();
   
   printf("Number of threads: %d",nthr);
-  #pragma omp parallel
+  #pragma omp parallel //reduction(+:pi)
   {
     double local=0;
     int i;
@@ -25,11 +25,11 @@ int main(){
       local+=f((i*h)+h/2);    
     }
     local=local*4*h;
-    //#pragma omp atomic
-    //pi+=local;
-
-    #pragma omp critical
+    #pragma omp atomic
     pi+=local;
+
+    //#pragma omp critical
+    //pi+=local;
   }
     double duration =omp_get_wtime()-tstart;
     printf("%lf in %lf sec\n", pi,duration);
