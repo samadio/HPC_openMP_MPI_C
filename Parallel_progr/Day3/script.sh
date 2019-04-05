8bf542c38d7c573cc7eba0ab991a13f398d13a9d
Simone Amadio <amadiosimone1996@gmail.com>
2019-04-05 16:29:47 +0200

    results done

--- Parallel_progr/Day3/script.sh
+++ Parallel_progr/Day3/script.sh
@@ -1,4 +1,10 @@
 cd /home/samadio/hpc/Parallel_progr/Day3
 module load openmpi
-mpirun -np 20 ./a.out >> results.txt
+
+for p in 1 2 4 8 16 20 40;do
+  for tries in 1 2;do
+    mpirun -np $p ./a.out >> results.txt
+  done
+  echo ""
+done
 exit
