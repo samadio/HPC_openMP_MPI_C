9266fc998b3558fc56dd346b79ecdb7bfa7da3dc
Simone Amadio <amadiosimone1996@gmail.com>
2019-04-07 18:49:09 +0200

    serial checked

--- Parallel_progr/Day2/ex1/parallel.c
+++ Parallel_progr/Day2/ex1/parallel.c
@@ -8,28 +8,28 @@ double f( double x){
 }
 
 int main(){
-  long unsigned int N=10000000000;
+  long unsigned int N=1000000000;
   double h=1.0/(N);
   double pi=0.0;
   int nthr= omp_get_num_threads();
   double tstart=omp_get_wtime();
   
-  #pragma omp parallel //reduction(+:pi)
-  {
+  //#pragma omp parallel //reduction(+:pi)
+  //{
     double local=0;
     long unsigned int i;
-    #pragma omp for schedule(static)
+   // #pragma omp for schedule(static)
     for( i= 0 ; i <= N-1; i++)
     {
       local+=f((i*h)+h/2);    
     }
     local=local*4*h;
-    #pragma omp atomic
-    pi+=local;
+   // #pragma omp atomic
+   // pi+=local;
 
    // #pragma omp critical
    //  pi+=local;
-  }
+  //}
     double duration =omp_get_wtime()-tstart;
     printf("%lf\n",duration);
 
