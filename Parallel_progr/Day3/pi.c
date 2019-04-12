e50af5239cf90302fb7043d188932cf9a30a957a
Simone Amadio <amadiosimone1996@gmail.com>
2019-04-12 14:48:36 +0200

    Boh

--- Parallel_progr/Day3/pi.c
+++ Parallel_progr/Day3/pi.c
@@ -37,7 +37,7 @@ int main(int argc,char *argv[]){
     double local=0.0;
     size_t work=N/npes;
     size_t rest= N-N/npes*npes; //number of elements lost in division
-    
+    size_t start,end;    
     if(rank<rest){
       size_t start=rank*(work+1); //to rest procs i give one more element
       size_t end=start+work+1;
--- Parallel_progr/Day3/script.sh
+++ Parallel_progr/Day3/script.sh
@@ -3,8 +3,8 @@ module load openmpi
 
 for p in 1 2 4 8 16 20 40;do
   for tries in 1 2;do
-    mpirun -np $p ./a.out >> results.txt
+    mpirun -np $p ./a.out >> results_balanced.txt
   done
-  echo ""
+  echo " "
 done
 exit
