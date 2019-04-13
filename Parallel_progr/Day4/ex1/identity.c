d96594e0681da72a5de1713feac920a073cf901f
Simone Amadio <amadiosimone1996@gmail.com>
2019-04-13 17:01:05 +0200

    matrix to array, to finish

--- Parallel_progr/Day4/ex1/identity.c
+++ Parallel_progr/Day4/ex1/identity.c
@@ -5,29 +5,21 @@
 int main(int argc,char *argv[]){
   int rank = 0; 
   int npes = 1;
-  int N=5; //global rows
+  int N=7; //global 
+  //FILE *f = fopen("identity.txt", "a"); //output file
   
   MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &npes );
     int local_N=N/npes ; //local numb of rows; 
-    int rest=N-(local_N*npes);
+    int rest=N%npes;
 
     if(rank<rest) local_N+=1;
 
     //allocate a matrix with local_N rows and N cols
-    int **A=(int**)malloc(local_N*sizeof(int*));
-    int** received;
-    
-    if(rank==0) received=(int**)malloc(local_N*sizeof(int*));   //only proc0 reserves memory
+    int *A=(int*)calloc(local_N*N,sizeof(int));
     
     int i,start;
-
-    for (i = 0; i < local_N; i++)
-    {
-      A[i]=(int*)calloc(N,sizeof(int));
-      if (rank==0) received[i]=(int*)malloc(N*sizeof(int)); //only proc0 reserves memory part2
-    }
     
     if(rank<rest) start=rank*(local_N);  //distributing workload
     else start=rest+(rank*local_N);
@@ -36,19 +28,16 @@ int main(int argc,char *argv[]){
     int row,col;
     for (i = 0; i <local_N; i++)
     {
-      A[i][start+i]=1; //row index always between 0 and local_N, col index depends on proc
+      A[i+N*(start+i)]=1; //row index always between 0 and local_N, col index depends on proc
     }
     
     MPI_Request req;
-    
-    FILE *f = fopen("identity.txt", "a"); //output file
 
 
     if (rank!=0) //every rank except 0 send even overlapping, each has its own length local_N
     {
-      MPI_Isend(A,N*local_N,MPI_INT,0,101,MPI_COMM_WORLD,&req);
+      MPI_Isend(&A,N*local_N,MPI_INT,0,101,MPI_COMM_WORLD,&req);
     }
-    
 
     if (rank==0) //0 receives them all
     {
@@ -57,33 +46,37 @@ int main(int argc,char *argv[]){
         {
           for (col = 0; col < N; col++)
           {
-            fprintf(f,"%d ",A[row][col]);  //0 write its own part of the matrix
+            printf("%d ",A[col+row*N]);  //0 write its own part of the matrix
           }
-          fprintf(f,"\n");
+          printf("\n");
         }
 
-      for (i = 1; i < npes; i++)
+      int* received;
+      for (i = 1; i <npes; i++)
       {
         if(i==rest) local_N=local_N-1;
-        MPI_Recv(received,N*local_N,MPI_INT,i,101,MPI_COMM_WORLD,MPI_STATUS_IGNORE);//&req);
+        received =(int*)malloc(N*local_N*sizeof(int));
+        MPI_Recv(&received,N*local_N,MPI_INT,i,101,MPI_COMM_WORLD,MPI_STATUS_IGNORE);//&req);
+        
         //MPI_Wait(&req,MPI_STATUS_IGNORE); //this way received can not be overwritten by another receive
 
+        
         for (row = 0; row < local_N; row++)
         {
           for (col = 0; col < N; col++)
           {
-            fprintf(f,"%d ",received[row][col]);
+            printf("%d ",received[col+row*N]);
           }
-          fprintf(f,"\n");
+          printf("by rank %d\n", i);
         }
+        free(received);
       }
-      
     }
-    
-    fclose(f);  
+
+    free(A);
 
   MPI_Finalize();
 
-
+//  fclose(f);  
   return 0;  
 }
