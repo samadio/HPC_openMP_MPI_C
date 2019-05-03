45b09ac5a3dafc2aaafbafef7d9ce75363210e1e
Simone Amadio <amadiosimone1996@gmail.com>
2019-05-03 14:11:28 +0200

    day5

--- Parallel_progr/Day4/ex1/identity_solution.c
+++ Parallel_progr/Day4/ex1/identity_solution.c
@@ -7,29 +7,34 @@ int main(int argc,char *argv[]){
   int npes = 1;
   int N=8; //global 
   FILE *f = fopen("identity.txt", "w"); //output file
-  
+
+//////////////No rest//////////////////
+
   MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &npes );
     int local_N=N/npes ; //local numb of rows; 
-    int rest=N%npes;
-
+   
     if(rank<rest) local_N+=1;
 
     //allocate a matrix with local_N rows and N cols
     int *A=(int*)calloc(local_N*N,sizeof(int));
     
-    int i,start;
+    int i,j,start;
     
     if(rank<rest) start=rank*(local_N);  //distributing workload
     else start=rest+(rank*local_N);
 
-
+    //for no rest, i_global=i+(rank*local_N)
     for (i = 0; i <local_N; i++)
     {
-      A[N*i+(start+i)]=1; //row index always between 0 and local_N, col index depends on proc
+        for ( j = 0; j <N; j++)
+        {
+            if(i_global==j)A[i][j]=1;
+        }        
     }
     
+
     if (rank!=0) //every rank except 0 send even overlapping, each has its own length local_N
     {
       MPI_Send(A,N*local_N,MPI_INT,0,101,MPI_COMM_WORLD);
@@ -49,11 +54,10 @@ int main(int argc,char *argv[]){
     
       int* received;
       received =(int*)malloc(N*local_N*sizeof(int)); //received has the maximum local_N, so it's fine
-      MPI_Request req;
       for (i = 1; i <npes; i++)
       {
         if(i==rest  && rest!=0) local_N=local_N-1;
-        MPI_Irecv(received,N*local_N,MPI_INT,i,101,MPI_COMM_WORLD,&req);
+        MPI_Recv(received,N*local_N,MPI_INT,i,101,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
         
         for (row = 0; row < local_N; row++)
         {
@@ -63,11 +67,6 @@ int main(int argc,char *argv[]){
           }
           fprintf(f,"by rank %d\n", i);
         }
-        MPI_Wait(&req,MPI_STATUS_IGNORE);
-        int* tmp;
-        tmp=received;
-        received=A;
-        A=tmp;
       }
       free(received);
     }
@@ -79,17 +78,16 @@ int main(int argc,char *argv[]){
   fclose(f);  
   return 0;  
 }
-
-
 ///////////////////////////////////overlapping/////////////////////
 
 
 ///every process has its own A
-/*
+
 if(rank==0){
     int* buffer=(int*) malloc(local_N*N*sizeof(int));
+    int count;
     MPI_Request req;
-         for(count=1; count<npes;count++){   
+         for(count=1; count<=npes;count++){   
             MPI_Irecv(buffer,N*local_N,MPI_INT,count,101,MPI_COMM_WORLD,&req);
             print(A); 
             MPI_Wait(&req,MPI_STATUS_IGNORE);
@@ -99,4 +97,3 @@ if(rank==0){
          }
          print(A); //last print missing
 }
-*/
