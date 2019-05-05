233860844fdf2d6695fafb9cb88f4b4ec8e196ca
Simone Amadio <amadiosimone1996@gmail.com>
2019-05-05 12:42:38 +0200

    square matrix mul done

--- Parallel_progr/Day5/matrix_mult.cu
+++ Parallel_progr/Day5/matrix_mult.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define N 4
-#define nthreads 2
+#define N 2048
+#define nthreads 512
 
 
 __global__ void matrix_mul (int* A, int*B,int*C,int size){
@@ -10,10 +10,10 @@ __global__ void matrix_mul (int* A, int*B,int*C,int size){
   int acc=0;
   int k;
   
-  for(int k=0; k<size;k++){
+  for(k=0; k<size;k++){
     acc+=A[rowidx+k]*B[colidx+k*size];
   }
-  B[i]=acc;
+  C[i]=acc;
 }
 
 
@@ -46,16 +46,16 @@ int main() {
   cudaMemcpy( dev_B, B, N*N*sizeof(int), cudaMemcpyHostToDevice );
 
   // launch matrix_mul kernel
-  transpose<<< (N*N)/nthreads, nthreads >>>(dev_A, dev_B, dev_C, N);
+  matrix_mul<<< (N*N)/nthreads, nthreads >>>(dev_A, dev_B, dev_C, N);
 
   // copy results
   cudaMemcpy( C, dev_C, N*N*sizeof(int),   cudaMemcpyDeviceToHost );
 
-  for(i=0;i<N*N;i++){
+/*  for(i=0;i<N*N;i++){
     if(i%N==0 && i!=0)printf("\n");
       printf("%d ", C[i]);
   }
-  printf("\n");
+  printf("\n"); */
 
   free(A); free(B);free(C);
   cudaFree( dev_A ); cudaFree( dev_B );cudaFree( dev_C );
