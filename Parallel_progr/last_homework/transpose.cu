a9e26f95759aa33d0a6b517a7b7f47143abade18
Simone Amadio <amadiosimone1996@gmail.com>
2019-05-11 20:30:38 +0200

    size_t

--- Parallel_progr/last_homework/transpose.cu
+++ Parallel_progr/last_homework/transpose.cu
@@ -3,109 +3,111 @@
 
 #define row 8192
 #define col 8192
-#define space 8192*8192*sizeof(int)
-#define elements 8192
+#define space 8192*8192*sizeof(size_t)
+#define elements 8192*8192
 
-#define th_per_block 64
+#define th_per_block 1024
 
 
-//this works until col<=Nthreads. In this case every threads moves only 1 data
-
-__global__ void initialize_table (int* A, int** table, int ncol){
-  int i=threadIdx.x;
-  table[i]=A+i*ncol;  
+global void initialize_table (size_t* A, size_t** table, size_t ncol){
+  size_t i=threadIdx.x;
+  table[i]=A+i*ncol;
 }
 
 
-__global__ void transpose (int** A, int** B,size_t cols){
+global void transpose (size_t** A, size_t** B,size_t cols){
   size_t i=blockIdx.x;
   size_t j=threadIdx.x;
   while(i<cols){
     B[j][i]=A[i][j];
-    i+=blockDim.x;
-  }
+  i+=blockDim.x;
+}
 
 }
 
-__global__ void fast_transpose (int** tableA, int** tableB, const size_t dim){
-  __shared__ int miniblockA[blockDim.x];
-  __shared__ int miniblockB[blockDim.x];
+global void fast_transpose (size_t** tableA, size_t** tableB, const size_t dim){
+  __shared__ size_t miniblockA[th_per_block];
+  __shared__ size_t miniblockB[th_per_block];
   if(threadIdx.x==0 && threadIdx.y==0){
-    size_t i;  
-    for(i=0;i<blockDim.x;i++){
-        miniblockA[i]=tableA[dim*blockIdx.y+i/dim][dim*blockIdx.x+i%dim];
-      }
+    size_t i;
+    for(i=0;i<th_per_block;i++){
+      miniblockA[i]=tableA[dim*blockIdx.y+i/dim][dim*blockIdx.x+i%dim];
+    }
   }
-  __sychthreads();
+  __syncthreads();
 
-  miniblockB[dim*threadIdx.x+threadIdx.x]=miniblockA[dim*threadIdx.x+threadIdx.y];
+  miniblockB[dim*threadIdx.y+threadIdx.x]=miniblockA[dim*threadIdx.x+threadIdx.y];
 
-  __sychthreads();
+  __syncthreads();
 
   if(threadIdx.x==0 && threadIdx.y==0){
-    for(i=0;i<blockDim.x;i++){
-      tableB[dim*blockIdx.y+i/dim][dim*blockIdx.x+i%dim]=miniblockB[i];
+    size_t i;
+    for(i=0;i<th_per_block;i++){
+      tableB[dim*blockIdx.x+i/dim][dim*blockIdx.y+i%dim]=miniblockB[i];
     }
   }
 
 }
 
 
-int main() {
-    
-  int*A=(int*)malloc(space);
-  int* dev_A;
-  int*B=(int*)malloc(space);
-  int* dev_B;
 
-  int i;
+
+size_t main() {
+
+  size_t*A=(size_t*)malloc(space);
+  size_t* dev_A;
+  size_t*B=(size_t*)malloc(space);
+  size_t* dev_B;
+  
+  size_t i;
   for(i=0;i<elements;i++){
     A[i]=i;
   }
-
-  int** dev_tableA;
-  int** dev_tableB;
-
+  
+  size_t** dev_tableA;
+  size_t** dev_tableB;
+  
   // allocate device copies of A and B
   cudaMalloc( (void**)&dev_A, space );
   cudaMalloc( (void**)&dev_B, space );
-  cudaMalloc( (void***)&dev_tableA, row*sizeof(int) );
-  cudaMalloc( (void***)&dev_tableB, col*sizeof(int) );
-
+  cudaMalloc( (void***)&dev_tableA, row*sizeof(size_t) );
+  cudaMalloc( (void***)&dev_tableB, col*sizeof(size_t) );
+  
   cudaMemcpy( dev_A, A, space, cudaMemcpyHostToDevice ); //send data to device
-
-  initialize_table<<< 1, row >>>(dev_A, dev_tableA,col); 
-  initialize_table<<< 1, col >>>(dev_B, dev_tableB,row); 
-
-
+  
+  initialize_table<<< 1, row >>>(dev_A, dev_tableA,col);
+  initialize_table<<< 1, col >>>(dev_B, dev_tableB,row);
+  
+  
   // launch transpose() kernel
-  transpose<<< elements/th_per_block, th_per_block >>>(dev_tableA, dev_tableB,col,dim); 
-
-  int dim= (int)sqrt(th_per_block);
+  transpose<<< elements/th_per_block, th_per_block >>>(dev_tableA, dev_tableB,col);
+  
+  size_t dim= (size_t)sqrt(th_per_block);
   dim3 grid,block;
   grid.x=col/dim;
   grid.y=row/dim;
   block.x=dim;
   block.y=dim;
-
-  fast_transpose<<< grid, block >>>(dev_tableA, dev_tableB,dim); 
-
+  
+  fast_transpose<<< grid, block >>>(dev_tableA, dev_tableB,dim);
+  
   // copy device result back to host copy of c
-  cudaMemcpy( B, dev_B, space,   cudaMemcpyDeviceToHost );
-
+  cudaMemcpy( B, dev_B, space, cudaMemcpyDeviceToHost );
+  
   for(i=0;i<elements;i++){
-    if(i%col==0 && i!=0)printf("\n");
-      printf("%d ", A[i]);
+  if(i%col==0 && i!=0)printf("\n");
+  printf("%d ", A[i]);
   }
   printf("\n");
-
+  
   for(i=0;i<elements;i++){
-    if(i%row==0 && i!=0)printf("\n");
-    printf("%d ", B[i]);
+  if(i%row==0 && i!=0)prsize_tf("\n");
+  
+  printf("%d ", B[i]);
   }
   printf("\n");
-
+  
   free(A); free(B);
-  cudaFree( dev_A ); cudaFree( dev_B ); cudaFree(tableA);cudaFree(tableB);
+  cudaFree( dev_A ); cudaFree( dev_B ); cudaFree(dev_tableA);cudaFree(dev_tableB);
   return 0;
-}
+  }
